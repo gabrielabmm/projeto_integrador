032d380015fa82c463cdeaff6bc6a02e96557b19
gabriela brasileiro <gabrielabrasileiro@discente.ufg.br>
2025-06-23 11:10:24 -0300

    Update login.go

--- SQL/testegabs/login.go
+++ SQL/testegabs/login.go
@@ -12,7 +12,6 @@ import (
 
 var db *sql.DB
 
-// Estrutura para armazenar as informações de um usuário
 type User struct {
 	ID       int
 	email    string
@@ -24,25 +23,25 @@ func conectar() (*sql.DB, error) {
 	return sql.Open("postgres", connStr)
 }
 
-// Função para verificar se o email existe no banco de dados
+
 func checkEmail(email string) (*User, error) {
 	var user User
 	err := db.QueryRow("SELECT id, email, password FROM users WHERE email = $1", email).Scan(&user.ID, &user.Email, &user.Password)
 	if err != nil {
 		if err == sql.ErrNoRows {
-			return nil, nil // Email não encontrado
+			return nil, nil 
 		}
-		return nil, err // Outro erro
+		return nil, err 
 	}
 	return &user, nil
 }
 
-// Função para realizar login
+
 func loginUser(c *gin.Context) {
 	email := c.DefaultPostForm("email", "")
 	password := c.DefaultPostForm("password", "")
 
-	// Verifica se o email existe no banco de dados
+	
 	user, err := checkEmail(email)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao verificar o email"})
@@ -54,27 +53,26 @@ func loginUser(c *gin.Context) {
 		return
 	}
 
-	// Verifica se a senha fornecida corresponde à senha armazenada
+	
 	if user.Password != password {
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Email ou senha incorretos"})
 		return
 	}
 
-	// Se o login for bem-sucedido, redireciona para o painel de controle ou outra página interna
 	c.JSON(http.StatusOK, gin.H{"message": "Login bem-sucedido", "user": user.Email})
 }
 
 func main() {
-	// Configuração do banco de dados
+	
 	setupDatabase()
 	defer db.Close()
 
-	// Inicialização do servidor Gin
+	
 	router := gin.Default()
 
-	// Rota para a tela de login
+	
 	router.POST("/login", loginUser)
 
-	// Inicialização do servidor
+	
 	router.Run(":8080")
 }
