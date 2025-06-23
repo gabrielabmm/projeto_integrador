0ff434a7c92f894c028f741d95188f89e0c067e3
Emilly Ranny <ranny.emilly@discente.ufg.br>
2025-06-23 15:00:40 -0300

    Create codigoemail.go

---
+++ projeto_integrador_versao_final/Internal/Handlers/codigoemail.go
@@ -0,0 +1,80 @@
+package handlers
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"regexp"
+	"strconv"
+	"time"
+)
+
+var lastReenvio = make(map[string]time.Time)
+
+type PageData struct {
+	Email     string
+	Codigo    string
+	Erro      string
+	Sucesso   bool
+	Countdown int
+	CanResend bool
+	Redirect  bool
+}
+
+func RenderCodigoPage(w http.ResponseWriter, r *http.Request) {
+	// Em produção, email deve vir de sessão ou query string
+	email := "teste@exemplo.com"
+	now := time.Now()
+
+	last := lastReenvio[email]
+	restante := 60 - int(now.Sub(last).Seconds())
+	if restante < 0 {
+		restante = 0
+	}
+
+	data := PageData{
+		Email:     email,
+		CanResend: restante == 0,
+		Countdown: restante,
+	}
+
+	if r.Method == http.MethodPost {
+		action := r.FormValue("action")
+		codigo := r.FormValue("codigo")
+		data.Codigo = codigo
+
+		if action == "verificar" {
+			match, _ := regexp.MatchString(`^\d{6}$`, codigo)
+			if !match {
+				data.Erro = "O código deve ter 6 dígitos numéricos."
+			} else if codigo == "123456" {
+				http.Redirect(w, r, "/escolher-nova-senha.html", http.StatusSeeOther)
+				return
+			} else {
+				data.Erro = "Código incorreto. Tente novamente."
+			}
+		} else if action == "reenviar" {
+			if restante == 0 {
+				lastReenvio[email] = now
+				data.Countdown = 60
+				data.CanResend = false
+				data.Sucesso = true
+			} else {
+				data.Erro = "Aguarde " + strconv.Itoa(restante) + "s para reenviar."
+			}
+		}
+	}
+
+	tmpl, err := template.ParseFiles("codigoemail.html")
+	if err != nil {
+		log.Println("Erro ao carregar template:", err)
+		http.Error(w, "Erro interno ao renderizar página", http.StatusInternalServerError)
+		return
+	}
+
+	tmpl.Execute(w, data)
+}
+
+func RegistrarRotasCodigoEmail() {
+	http.HandleFunc("/codigoemail", RenderCodigoPage)
+}
